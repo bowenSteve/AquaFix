423136f44a3ea261ea6b124eefd6a3fd96da6fa4
stephenbowen <bowensteven17@gmail.com>
2024-09-03 12:38:35 +0300

    updates

--- server/models.py
+++ server/models.py
@@ -18,6 +18,16 @@ class User(db.Model):
     profile = db.relationship('Profile', backref='user', uselist=False)
     plumber_details = db.relationship('PlumberDetail', backref='user', uselist=False)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
 
 class Profile(db.Model):
     __tablename__ = 'profiles'
