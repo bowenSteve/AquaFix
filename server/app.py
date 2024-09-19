2830c53208805f5e90c67d546f4d91ba2b24dbdf
stephenbowen <bowensteven17@gmail.com>
2024-09-19 09:19:33 +0300

    updates

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ from .models import db, User, Profile, PlumberDetail
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:password@localhost/aqua_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///aqua_db.sqlite"
 app.config["JWT_SECRET_KEY"] = "fsbdgfnhgvjnvhmvh" + str(random.randint(1, 1000000000000))
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
 app.config["SECRET_KEY"] = "JKSRVHJVFBSRDFV" + str(random.randint(1, 1000000000000))
